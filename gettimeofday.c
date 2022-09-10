99a4c3c15da46970eb0f1dad40e4bac33b0655e5
Angelinne <kayumi85@hotmail.com>
2022-09-10 15:01:41 +0900

    new

---
+++ gettimeofday.c
@@ -0,0 +1,22 @@
+//gettimeofdayは時刻とタイムゾーンを取得または設定する。
+//gettimeofday() と settimeofday()
+//は成功すると 0 を返し、失敗した場合は -1 を返す
+//c言語でコードブロック内の経過時間を計算するために、gettimeofdayを使う。
+
+#include <sys/time.h>
+#include <stdio.h>
+
+//int gettimeofday(struct timeval *tv, struct timezone *tz);
+int main (void)
+{
+	struct timeval start;
+	struct timeval end;
+	int	i;
+
+	gettimeofday(&start, NULL);
+	i = 0;
+	while(i < NULL)
+	{
+		i++;
+	}
+	printf("%d\n", gettimeofday(&end, NULL));
