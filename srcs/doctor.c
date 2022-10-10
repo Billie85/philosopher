26ca91e9ca16d2878bdcf4fda8580a31bd2a6359
Angelinne <kayumi85@hotmail.com>
2022-10-10 19:54:11 +0900

    add libft

--- srcs/doctor.c
+++ srcs/doctor.c
@@ -14,22 +14,22 @@ void *doctor(void *data)
 	{
 		i = 0;
 		time_now = get_time();
-		//pthread_mutex_lock(&args->mutex);
-		// while(i < philo_data->two_way->number_of_philosophers)//5
-		// {
-		// 	printf("[%ld\n]",args->philo[i].time_finish_eat);
-		// 		printf("%d\n", i);
-		// 		fflush(stdout);
-		// 	if (time_now - args->philo[i].time_finish_eat > args->time2die)
-		// 	{
-		// 		printf("aaaaaaaa\n");
-		// 		//printf("//// [%ld\n]",args->philo[i].time_finish_eat);
-		// 		//rgs->is_dead = 1;//ここで死んだことを教える。
-		// 		//printf("%ld %ld is DEAD\n", philo_data->two_way->time_dead ,philo_data->philo_id);
-		// 		//return(NULL);
-		// 	}
-		// 	i++;
-		// }
+		pthread_mutex_lock(&args->mutex);
+		 while(i < philo_data->two_way->number_of_philosophers)//5
+		 {
+			printf("[%ld\n]",args->philo[i].time_finish_eat);
+		 	printf("%d\n", i);
+		 	fflush(stdout);
+		 	if (time_now - args->philo[i].time_finish_eat > args->time2die)
+		 	{
+		 		printf("aaaaaaaa\n");
+				//printf("//// [%ld\n]",args->philo[i].time_finish_eat);
+				//rgs->is_dead = 1;//ここで死んだことを教える。
+				//printf("%ld %ld is DEAD\n", philo_data->two_way->time_dead ,philo_data->philo_id);
+				//return(NULL);
+			}
+			i++;
+		}
 	}
 	//pthread_mutex_unlock(&args->mutex);
 }
