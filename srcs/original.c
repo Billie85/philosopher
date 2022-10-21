df4a2c7a56679484de648cb694653168171d781a
Angelinne <kayumi85@hotmail.com>
2022-10-22 03:26:58 +0900

    delete

--- srcs/original.c
+++ srcs/original.c
@@ -11,13 +11,3 @@ int	my_strcmp(char *s, char *s1)
 	}
 		return(s[i] - s1[i]);
 }
-
-
-/* int main(void)
-{
-	int hi;
-	char s[10] = "THINK";
-	char s1[10] = "THINKaaaa";
-	hi = my_strcmp(s, s1);
-	printf("%d\n", hi);
-} */
